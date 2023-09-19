7fdc10c4657b6ba37844b25cdfd831e8dc6aaa91
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2023-09-19 23:32:27 +0530

    added server and client

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,20 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useEffect } from 'react';
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
+// import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
+  useEffect(()=>{
+    fetch('/api').then(res=>res.json()).then(console.log)
+  })
   return (
     <div>
-      <NxWelcome title="client" />
+      {/* <NxWelcome title="client" /> */}
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
       <br />
       <div role="navigation">
         <ul>
