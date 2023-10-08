b4e6afb2c3b5e664867db9c7252c3beeb14db49e
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2023-10-08 12:03:02 +0530

    chore: updated pre commit hooks

--- apps/client/src/app/app.spec.tsx
+++ apps/client/src/app/app.spec.tsx
@@ -13,13 +13,4 @@ describe('App', () => {
     );
     expect(baseElement).toBeTruthy();
   });
-
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    expect(getByText(/Welcome client/gi)).toBeTruthy();
-  });
 });
