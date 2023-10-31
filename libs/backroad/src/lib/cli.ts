959745d70c1469c809d380e0b16e6303565a6323
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2023-10-31 23:28:28 +0530

    fix: removed commander dependency

--- libs/backroad/src/lib/cli.ts
+++ libs/backroad/src/lib/cli.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-const program = new Command();
+// import { Command } from 'commander';
+// const program = new Command();
 console.log('running backroad cli');
-program.name('backroad-core').description('Run a backroad script in dev mode');
+// program.name('backroad-core').description('Run a backroad script in dev mode');
 
-program.command('push').action(() => {
-  console.log('coming soon 💫');
-});
-program.parse();
+// program.command('push').action(() => {
+//   console.log('coming soon 💫');
+// });
+// program.parse();
